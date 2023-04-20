ca7896cfd23e61d4ad90c45c44509a8ec7fd2534
aswarthana <banr20@gmail.com>
2023-04-20 21:57:05 +0400

    dar-mode feature and switch added.

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,12 @@
 import { HStack, Image, Text } from "@chakra-ui/react";
 import logo from "../assets/logo.webp";
+import ColorModeSwitch from "./ColorModeSwitch";
 
 const NavBar = () => {
   return (
-    <HStack>
+    <HStack justifyContent={"space-between"} padding={2}>
       <Image src={logo} boxSize="50px"></Image>
-      <Text>Nav Bar</Text>
+      <ColorModeSwitch />
     </HStack>
   );
 };
