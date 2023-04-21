875c69be95a5363989628ef9a72ed1b7ddbff391
aswarthana <banr20@gmail.com>
2023-04-21 12:12:22 +0400

    games grid added in main page

--- src/apiClient.ts
+++ src/apiClient.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://api.rawg.io/api/games",
+  baseURL: "https://api.rawg.io/api",
   params: {
     key: "d956f2efaa734cc593b12ef521e4d9fc",
   },
---
+++ src/components/GameGrid.tsx
@@ -0,0 +1,38 @@
+import { useEffect, useState } from "react";
+import apiClient from "../apiClient";
+import { Center, Text } from "@chakra-ui/react";
+
+interface Game {
+  id: number;
+  name: string;
+}
+
+interface GamesResponse {
+  count: number;
+  results: Game[];
+}
+
+const GameGrid = () => {
+  const [games, setGames] = useState<Game[]>();
+  const [error, setError] = useState<string>("");
+
+  useEffect(() => {
+    apiClient
+      .get<GamesResponse>("/games")
+      .then((response) => setGames(response.data.results))
+      .catch((error) => setError(error.message));
+  });
+
+  return (
+    <Center>
+      {error && <Text color="red"> {error}</Text>}
+      <ul>
+        {games?.map((game) => (
+          <li key={game.id}>{game.name}</li>
+        ))}
+      </ul>
+    </Center>
+  );
+};
+
+export default GameGrid;
