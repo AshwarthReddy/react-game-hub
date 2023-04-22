4757a061d934396e8c7a565dc81901a159880466
aswarthana <banr20@gmail.com>
2023-04-22 22:26:11 +0400

    display genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,14 +1,25 @@
+import { HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
 import useGares from "../hooks/useGenres";
+import getCroppedImgaeUrl from "../imageUrlBuilder";
 
 const GenreList = () => {
   const { data } = useGares();
   return (
     <>
-      <ul>
+      <List>
         {data?.map((genre) => (
-          <li key={genre.id}>{genre.name}</li>
+          <ListItem paddingY="1" key={genre.id}>
+            <HStack>
+              <Image
+                boxSize="32px"
+                borderRadius={8}
+                src={getCroppedImgaeUrl(genre.image_background)}
+              />
+              <Text fontSize="xl">{genre.name}</Text>
+            </HStack>
+          </ListItem>
         ))}
-      </ul>
+      </List>
     </>
   );
 };
